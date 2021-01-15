884a5adcf9375df9880737b51745f83783f983ad
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-01-15 12:52:13 -0500

    added some GET routes, added Heroku

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs")
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//code3 here:
+//GET routes:
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+app.get ('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+
+
+  //=========================================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT" + PORT);
+})
