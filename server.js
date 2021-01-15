cd8876c136d2c7a66b29062d63a9811e3aae8653
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-01-15 16:19:50 -0500

    latest update

--- server.js
+++ server.js
@@ -11,17 +11,18 @@ app.use(express.static("public"));
 
 //code3 here:
 //GET routes:
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
-// });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+});
 
 
 app.get ('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
 });
 
 
