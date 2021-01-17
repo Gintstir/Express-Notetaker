6ed8f4e2ac6f2704ea57789eed8afc9295163f00
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-01-17 00:41:10 -0500

    clean up time

--- routes/apiroutes/routes.js
+++ routes/apiroutes/routes.js
@@ -4,6 +4,8 @@ const { saveNote, deleteNote } = require('../../lib/notes');
 
 const notes = require('../../db/db.json');
 
+const fs = require('fs');
+
 //==========================================
 
 router.get('/notes', (req, res) => {
@@ -20,24 +22,23 @@ router.post('/notes', (req, res) => {
     res.json(notes);
 });
 
-router.delete('/notes', (req, res) => {
-    notes = deleteNote(req.params.id);
-    res.json(notes);
-})
+//BONUS:
+//Delete a note from the notes array based on ID
+router.delete("/notes/:id", (req, res) => {
+    //find the id of the note that is going to be deleted
+    let deleteNote = notes.findIndex((item) => item.id === req.params.id);
+    //remove the note from the notes array
+    notes.splice(deleteNote, 1);
+  
+    //write the updated array to db.json
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2), function (
+      err
+    ) {
+      if (err) throw err;
+    });
+    res.json({ deletion: "Note Deleted!" });
+  });
 
-// router.post("/notes", (req, res) => {
-//     const notes = JSON.parse(
-//       fs.readFileSync(path.join(__dirname, "../Develop/db/db.json"), "utf8")
-//     );
-//     req.body.id = notes.length.toString();
-//     const note = req.body;
-//     notes.push(note);
-//     fs.writeFileSync(
-//       path.join(__dirname, "../develop/db/db.json"),
-//       JSON.stringify(notes, null, 2)
-//     );
-//     res.json(notes);
-//   });
 
 //==============================================
 
